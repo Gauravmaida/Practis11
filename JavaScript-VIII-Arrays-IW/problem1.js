fadd320649d3f632ca7302810060d6b30de45d68
Gaurav-0021 <108812151+Gaurav-0021@users.noreply.github.com>
2022-10-13 10:11:10 +0530

    Array

---
+++ JavaScript-VIII-Arrays-IW/problem1.js
@@ -0,0 +1,9 @@
+// Problem 1 : Given an array print the position (starting with 1) and the element in a single line.
+
+
+let arr = ["naman","chamna","lakhan","bhuvan"];
+let bag = ""
+for(let i = 0; i<arr.length; i++){
+  bag +=arr[i]+ " ";
+}
+console.log(bag);
